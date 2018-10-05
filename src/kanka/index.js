d6ec0d4609f8b9b103877a2aa49d5fe86227fcbf
James Firth <hello@james-firth.com>
2018-10-04 21:37:47 -0500

    Move library to a sub-folder to allow for cross node-web support in the future

--- src/kanka/index.js
+++ src/kanka/index.js
@@ -3,7 +3,6 @@ const campaign = require('./campaign');
 const Character = require('./character').Character;
 const User = require('./user').User;
 
-
 module.exports = {
     setToken: api.setToken,
     listCampaigns: campaign.all,
